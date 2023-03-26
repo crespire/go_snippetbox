d746c4025608de3052f728c467bdd8225fe33c06
crespire <hello@crespire.dev>
2023-03-26 11:19:00 -0400

    finish chapter 2.5

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
+  "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
   w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  if err != nil || id < 1 {
+    http.NotFound(w, r)
+    return
+  }
+
+  fmt.Fprintf(w, "Display snippet %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    w.Header().Set("Allow", http.MethodPost)
+    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    return
+  }
+
+  w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
 
   port := ":4000"
   log.Print("Starting server on ", port)
